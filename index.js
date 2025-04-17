7aa0db091bc36e69c8278300bd78789aedfafb92
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-16 21:41:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://cybriahb.pages.dev/';
+const nggUrl = 'https://allwebgames.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'cybriahb.pages.dev') {
+    if (req.headers.host === 'allwebgames.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
