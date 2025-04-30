607769f4bf8802e97af8b0afd64a5c96e15663cf
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-29 22:08:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://assets.shuttleproxy.com/';
+const nggUrl = 'https://help-me.undo.cl/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'allwebgames.com') {
+    if (req.headers.host === 'help-me.undo.cl') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
